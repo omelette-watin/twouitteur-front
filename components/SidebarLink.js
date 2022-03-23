2e2787e16920833407d6addd191807e888b4273a
Nicolas <pnwa@icloud.com>
2022-03-23 22:31:15 +0100

    basic sidebar mobile first

---
+++ components/SidebarLink.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+const SidebarLink = ({ Icon, text, active, link = "/" }) => {
+  return (
+    <Link href={link}>
+      <a className="block">
+        <div
+          className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation ${
+            active && "font-bold"
+          }`}
+        >
+          <Icon className="h-7" />
+          <span className="hidden xl:inline">{text}</span>
+        </div>
+      </a>
+    </Link>
+  )
+}
+
+export default SidebarLink
