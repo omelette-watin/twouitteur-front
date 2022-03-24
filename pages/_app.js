bcb1b56c87d6a3d4de39eb93580354989c026f0c
Nicolas <pnwa@icloud.com>
2022-03-24 13:44:52 +0100

    Loading component

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
 import "../styles/globals.css"
+import { useState } from "react"
+import Loading from "../components/Loading"
 
 function MyApp({ Component, pageProps }) {
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  if (loading) {
+    return <Loading />
+  }
+
   return <Component {...pageProps} />
 }
 
