bcdfbe4ef1c66a2efbc6b12c3a29aa0b27e283b7
Nicolas <pnwa@icloud.com>
2022-03-26 00:27:59 +0100

    basic axios api setup

---
+++ services/api.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+const api = axios.create({
+  baseURL: process.env.API_URL || "http://localhost:5000",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+})
+
+export default api
