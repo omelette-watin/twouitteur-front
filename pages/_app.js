275b970f585f3cab7aa0fe17c7af622dd2fc7ecb
Nicolas <pnwa@icloud.com>
2022-03-25 23:40:30 +0100

    reset for auth context

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,6 @@
 import "../styles/globals.css"
-import { useState } from "react"
-import Loading from "../components/Loading"
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
-
-  if (loading) {
-    return <Loading />
-  }
-
   return <Component {...pageProps} />
 }
 
