03a22945d35f99d9b66c3c35803ed10782dd4e8b
Nicolas <pnwa@icloud.com>
2022-03-26 20:35:17 +0100

    connection screen if user is not authenticated

---
+++ components/ConnectionScreen.js
@@ -0,0 +1,41 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const ConnectionScreen = () => {
+  return (
+    <div
+      className={
+        "min-h-screen flex flex-col items-center justify-center text-white xl:flex-row"
+      }
+    >
+      <Image
+        src={"/twouitteur-white.svg"}
+        width={50}
+        height={50}
+        alt={"twouitteur logo"}
+      />
+      <div className={"text-3xl my-10 xl:mr-[80px] xl:ml-8"}>
+        We were waiting for you !
+      </div>
+      <div className="space-y-3 flex flex-col items-center">
+        <Link href={"/login"}>
+          <a>
+            <button className="block bg-[#1d9bf0] rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8] hover:scale-95 transition duration-200 ease-out">
+              Sign-in{" "}
+            </button>
+          </a>
+        </Link>
+        <div className="text-l">Pas encore de compte ?</div>
+        <Link href={"/register"}>
+          <a>
+            <button className="block bg-white text-black rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-slate-300 hover:scale-95 transition duration-200 ease-out">
+              Sign-out{" "}
+            </button>
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default ConnectionScreen
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,49 @@
 import "../styles/globals.css"
+import { useEffect, useState } from "react"
+import Loading from "../components/Loading"
+import api from "../services/api"
+import { AuthContext } from "../contexts/auth"
+import ConnectionScreen from "../components/ConnectionScreen"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+  const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    ;(async () => {
+      const token = localStorage.getItem("token")
+
+      if (token) {
+        try {
+          const me = await api.get("/user/me")
+
+          if (me) {
+            setUser(me)
+            setLoading(false)
+          }
+        } catch (e) {
+          localStorage.removeItem("token")
+          setLoading(false)
+        }
+      }
+
+      setLoading(false)
+    })()
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (!user && pageProps.protected) {
+    return <ConnectionScreen />
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <Component {...pageProps} />
+    </AuthContext.Provider>
+  )
 }
 
-export default MyApp
+export default App
