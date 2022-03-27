207b3d5db7e847d3ed559ba942179793a4e0ba76
Nicolas <pnwa@icloud.com>
2022-03-27 01:38:47 +0100

    fix tailwind error with size

--- components/Loading.js
+++ components/Loading.js
@@ -1,8 +1,8 @@
-const Loading = ({ color, size = 8 }) => {
+const Loading = ({ color }) => {
   return (
     <svg
       role="status"
-      className={`ml-4 w-${size} h-${size} text-gray-200 animate-spin dark:text-gray-600 inline`}
+      className="ml-4 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 inline"
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
