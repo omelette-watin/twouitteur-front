cdf6c7c7d64ad24ccdd9c59a08d2c576edc18666
Nicolas <pnwa@icloud.com>
2022-04-05 19:51:19 +0200

    changed sticky z-index

--- components/Feed.js
+++ components/Feed.js
@@ -20,7 +20,7 @@ const Feed = () => {
 
   return (
     <div className="text-white flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
-      <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-3 border-b border-gray-700">
+      <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 bg-black sticky top-0 z-50 border-b border-gray-700">
         <div className={"flex items-center space-x-4"}>
           {window.location.pathname.includes("/status") ? (
             <ArrowLeftIcon
