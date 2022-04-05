2346ca9cd178706eee1163bc77a27cbc8f2f981f
Nicolas <pnwa@icloud.com>
2022-04-05 17:38:31 +0200

    disabled while submitting

--- components/TweetBox/SubmitButton.jsx
+++ components/TweetBox/SubmitButton.jsx
@@ -1,6 +1,6 @@
 import { useForm } from "usetheform"
 
-export const SubmitButton = () => {
+export const SubmitButton = ({ submitting }) => {
   const { isValid, pristine, state } = useForm()
   const isEmpty = !state.editor?.plainText && !state.media && !state.gif
 
@@ -9,7 +9,7 @@ export const SubmitButton = () => {
       className={
         "py-1 px-4 outline-none border-0 bg-[#1d9bf0] hover:bg-[#1a8cd8] rounded-2xl flex items-center cursor-pointer disabled:cursor-default disabled:bg-[#8ED0F9FF]"
       }
-      disabled={!isValid || pristine || isEmpty}
+      disabled={!isValid || pristine || isEmpty || submitting}
       type="submit"
     >
       <span>Tweet</span>
