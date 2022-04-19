ea52436a62839c6c596dffdfca4b0edff9d39e5b
Nicolas <pnwa@icloud.com>
2022-04-19 12:32:55 +0200

    long format date component

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,11 +1,16 @@
 import ReactTimeAgo from "react-time-ago"
+import { parseISO, format } from "date-fns"
 
-const Date = ({ date }) => {
+const Date = ({ date, full }) => {
   return (
     <time>
-      <ReactTimeAgo date={date} locale="en-US" timeStyle="twitter" />
+      <ReactTimeAgo date={date} locale="en-US" timeStyle={!full && "twitter"} />
     </time>
   )
+}
+
+export const FullDate = ({ date }) => {
+  return <time>{format(parseISO(date), "H:m · MMM d, y")}</time>
 }
 
 export default Date
